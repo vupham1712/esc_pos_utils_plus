edb6e058577e1fea10b0ea1b38414376f11cd87e
Vu Phan <vu.pham.levincigroup.com>
2025-01-01 23:56:44 +0700

    Try change font size

--- lib/src/enums.dart
+++ lib/src/enums.dart
@@ -1,6 +1,9 @@
 enum PosAlign { left, center, right }
+
 enum PosCutMode { full, partial }
+
 enum PosFontType { fontA, fontB }
+
 enum PosDrawer { pin2, pin5 }
 
 /// Choose image printing function
@@ -21,7 +24,7 @@ class PosTextSize {
   static const size8 = PosTextSize._internal(8);
 
   static int decSize(PosTextSize height, PosTextSize width) =>
-      16 * (width.value - 1) + (height.value - 1);
+      12 * (width.value - 1) + (height.value - 1);
 }
 
 class PaperSize {
